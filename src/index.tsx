4a0379fbe7565819c761c0d493692e9a85bd254a
muratk <kupenovmurat@gmail.com>
2022-12-15 01:09:44 +0600

    add react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './layout/Login/Login'
+import Registration from './layout/Registration/Registration'
+
 import './index.css'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    },
+    {
+        path: '/login',
+        element: <Login />,
+    },
+    {
+        path: '/registration',
+        element: <Registration />,
+    },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+       <RouterProvider router={router} />
     </React.StrictMode>
 )
--- src/layout/Main/Main.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from 'react'
-
-const Main: FC<any> = () => {
-    return <div>Main</div>
-}
-
-export default Main
