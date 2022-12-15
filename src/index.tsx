16ab1e954258628e046b1993dcd421ea8436564b
muratk <kupenovmurat@gmail.com>
2022-12-15 22:11:00 +0600

    fix react-router & add normalize css

--- src/index.tsx
+++ src/index.tsx
@@ -5,25 +5,29 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './layout/Login/Login'
 import Registration from './layout/Registration/Registration'
 
+import 'normalize.css'
+
 import './index.css'
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
-    },
-    {
-        path: '/login',
-        element: <Login />,
-    },
-    {
-        path: '/registration',
-        element: <Registration />,
+        children: [
+            {
+                path: 'login',
+                element: <Login />,
+            },
+            {
+                path: 'registration',
+                element: <Registration />,
+            },
+        ],
     },
 ])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-       <RouterProvider router={router} />
+        <RouterProvider router={router} />
     </React.StrictMode>
 )
--- src/layout/Login/Login.tsx
+++ src/layout/Login/Login.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 
 const Login: FC<any> = () => {
-    return <div>Login</div>
+    return <h1>Login</h1>
 }
 
 export default Login
