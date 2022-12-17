c00a7caeed9d0136967e195c27c7b06ae172eef6
muratk <kupenovmurat@gmail.com>
2022-12-17 23:52:14 +0600

    add Pages

---
+++ src/pages/Terms/Terms.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+import { Outlet } from 'react-router-dom'
+import styles from './Terms.module.css'
+
+const Terms: FC<any> = () => {
+    return (
+        <div className="wrapper">
+            <h1>Terms of service</h1>
+        </div>
+    )
+}
+
+export default Terms
